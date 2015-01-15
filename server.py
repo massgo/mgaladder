c4281d074e0b484ff6a7776e749e07165c359dca
hwchen <walther.chen@gmail.com>
2015-01-15 12:32:20 -0500

    Flask app setup
    - create Server.py
    - create /static/index.html, links to results.js for dynamic ladder
    - create /static/js with react and jquery libraries
    - add    /static/js/results.js react file for dynamic rendering of results list
    
    - add '/' route to Server, serves static index.html for now
    - add '/results' route to server, GET returns json {results:[<list of results>]}
    - add ResultsBox and ResultsList (renders list dynamically) containers to results.js
    - add handle componentDidMount which gets initial JSON data from route '/results'

---
+++ server.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+# Flask server for MGA ladder
+
+from flask import Flask, jsonify
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def index():
+    """ serves static file for testing purposes"""
+    return app.send_static_file('index.html') 
+
+@app.route('/results')
+def results():
+    resultsList = {"results": [
+        {"black": "Walther", "white": "Andrew"},
+        {"black": "Walther", "white": "Chun"}
+        ]}
+    return jsonify(resultsList)
+
+if __name__ == '__main__':
+    app.run(debug=True)
